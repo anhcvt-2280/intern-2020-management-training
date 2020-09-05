fb3831a0fdd81caac67f9521173301c55e385de6
anhcvt-2280 <vu.tuan.anhc@sun-asterisk.com>
2020-09-05 22:50:54 +0700

    Add new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  USER_PARAMS_PERMIT = %i(name email password).freeze
+  USER_PARAMS_PERMIT = %i(name email password date_of_birth address gender
+    program_language_id position_id department_id school_id office_id
+    image password_confirmation).freeze
   VALID_EMAIL_REGEX = Settings.REGEX.model.user.email
 
   has_many :user_courses, dependent: :destroy
@@ -15,6 +17,17 @@ class User < ApplicationRecord
   validates :password, presence: true,
             length: {minimum: Settings.validates.model.user.pwd.min_length},
             allow_nil: true
+  validates :address,
+            length: {maximum: Settings.validates.model.user.name.max_length}
+  validates :program_language_id, presence: true
+  validates :position_id, presence: true
+  validates :department_id, presence: true
+  validates :school_id, presence: true
+  validates :office_id, presence: true
+  validates :date_of_birth, presence: true
+  validate :birthday_cannot_be_in_future, :birthday_old_men
+
+  mount_uploader :image, UserUploader
 
   enum role: {trainee: 0, trainer: 1}, _prefix: true
 
@@ -22,4 +35,20 @@ class User < ApplicationRecord
   scope :exclude_ids, ->(ids){where.not id: ids if ids.present?}
 
   has_secure_password
+
+  private
+
+  def birthday_cannot_be_in_future
+    return unless date_of_birth > Time.zone.today
+
+    errors.add :date_of_birth,
+               I18n.t("trainers.users.new.error_birthday_in_future")
+  end
+
+  def birthday_old_men
+    return unless date_of_birth < 100.years.ago
+
+    errors.add :date_of_birth,
+               I18n.t("trainers.users.new.error_birthday_in_oldmen")
+  end
 end
